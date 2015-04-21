ee85e97d8422f017bd1f0562bfd7355fd32c3e8b
kim <honkang@163.com>
2015-04-21 14:44:54 +0800

    init

---
+++ MemcacheTestPro/src/demo.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: tencent
+ * Date: 15/4/21
+ * Time: 14:34
+ */
+var_dump("start<br/>");
+$memcache = memcache_connect('10.66.108.24', 9101);
+
+if ($memcache) {
+    $memcache->set("str_key", "String to store in memcached");
+    $memcache->set("num_key", 123);
+
+    $object = new StdClass;
+    $object->attribute = 'test';
+    $memcache->set("obj_key", $object);
+
+    $array = Array('assoc'=>123, 345, 567);
+    $memcache->set("arr_key", $array);
+
+    var_dump($memcache->get('str_key'));
+    var_dump($memcache->get('num_key'));
+    var_dump($memcache->get('obj_key'));
+}
+else {
+    echo "Connection to memcached failed";
+})
